49eb5e79dc11a556f81a34cfcba7c15947ddd8cf
Times-Two <twotimes678@gmail.com>
2025-05-01 15:12:37 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,24 @@
-const CACHE_NAME = "calculator-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./calculate.css",
-  "./app.js",
-  "./manifest.json",
-  "./theo.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache).catch((err) => {
-        console.warn("Some files failed to cache:", err);
-      });
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+      caches.open("calculator-cache").then((cache) => {
+        return cache.addAll([
+          "./",
+          "./index.html",
+          "./calculate.css",    // Update if your CSS file has a different name
+          "./app.js",    // Update if your JS file has a different name
+          "./manifest.json",
+          "./theo.png",
+          "./theo.png"
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (e) => {
+    e.respondWith(
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
